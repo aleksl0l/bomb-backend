ed87a38f4c0346461211892536c70a382d0e5d18
Vasilev Maxim <vsmaxim8@gmail.com>
2019-04-20 01:42:11 +0300

    implemented default map filling logic

--- game-map/map.go
+++ game-map/map.go
@@ -1,6 +1,33 @@
 package game_map
 
-// Test me pls
+import (
+	"github.com/aleksl0l/bomb-backend/game-object"
+	"github.com/aleksl0l/bomb-backend/player"
+)
 
 type GameMap interface {
+	setCell(target *GameMap, obj game_object.GameObject)
+	setPlayer(target *GameMap, obj player.Player)
+}
+
+type NaiveMap struct {
+	objects []game_object.GameObject
+	field   [][]int
+}
+
+func NewNaiveMap(size int) NaiveMap {
+	newField := make([][]int, size)
+
+	for i := range newField {
+		newField[i] = make([]int, size)
+	}
+
+	// Fill field with default bomberman pattern
+	// https://st.emulroom.com/images/dendy/bomberman/bomberman-(4).png
+	for i := 1; i < size-1; i += 2 {
+		for j := 1; j < size-1; j += 2 {
+			newField[i][j] = -1
+		}
+	}
+	return NaiveMap{make([]game_object.GameObject, 0), newField}
 }
--- game-object/game_object.go
+++ game-object/game_object.go
@@ -1,4 +1,12 @@
 package game_object
 
-type GameObject interface {
+type Point struct {
+	x uint16
+	y uint16
+}
+
+type GameObject struct {
+	Id       int
+	Type     string
+	Position Point
 }
