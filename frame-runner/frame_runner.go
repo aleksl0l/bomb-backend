29f1e6685f0a3a549c40f2537015e6242ba3dc24
aleksl0l <aleksl0l@yandex.ru>
2019-04-21 00:41:34 +0300

    Add FrameRunner draft implementation

--- frame-runner/frame_runner.go
+++ frame-runner/frame_runner.go
@@ -1,4 +1,27 @@
 package frame_runner
 
-type FrameRunner interface {
+import (
+	"github.com/aleksl0l/bomb-backend/game-map"
+	"github.com/aleksl0l/bomb-backend/player"
+	"github.com/gorilla/websocket"
+)
+
+type IFrameRunner interface {
+	broadcastMap()
+}
+
+type FrameRunner struct {
+	Players []player.IPlayer
+	Map     game_map.GameMap
+	Conn    websocket.Conn
+}
+
+func NewFrameRunner() {
+
+}
+
+func (fr *FrameRunner) broadcastMap() {
+	for i := 0; i < len(fr.Players); i++ {
+		fr.Players[i].SendMap(fr.Map)
+	}
 }
