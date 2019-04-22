b526e07aed80fbee0ed0aade1c9bc2f3381a28d2
aleksl0l <aleksl0l@yandex.ru>
2019-04-22 23:19:58 +0300

    Update FrameRunner

--- frame-runner/frame_runner.go
+++ frame-runner/frame_runner.go
@@ -1,27 +1,76 @@
 package frame_runner
 
 import (
+	"fmt"
 	"github.com/aleksl0l/bomb-backend/game-map"
+	"github.com/aleksl0l/bomb-backend/message"
 	"github.com/aleksl0l/bomb-backend/player"
-	"github.com/gorilla/websocket"
+	"github.com/golang/protobuf/proto"
+	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 type IFrameRunner interface {
-	broadcastMap()
+	BroadcastMap()
+	AddPlayer(player player.IPlayer)
+	Run()
+	prepareMapResponse() ([]byte, error)
 }
 
 type FrameRunner struct {
 	Players []player.IPlayer
 	Map     game_map.GameMap
-	Conn    websocket.Conn
 }
 
-func NewFrameRunner() {
-
+func NewFrameRunner() IFrameRunner {
+	return &FrameRunner{
+		Players: make([]player.IPlayer, 0, 10),
+		Map:     game_map.NewNaiveMap(10),
+	}
 }
 
-func (fr *FrameRunner) broadcastMap() {
+func (fr *FrameRunner) prepareMapResponse() ([]byte, error) {
+	naiveMap := fr.Map.(*game_map.NaiveMap)
+	gameMapResponse := &message.GameMap{
+		GameObjects: naiveMap.Objects,
+		Players:     make([]*message.Player, 0, len(fr.Players)),
+	}
+	for _, p := range fr.Players {
+		gameMapResponse.Players = append(gameMapResponse.Players, &p.(*player.Player).Player)
+	}
+	content, err := proto.Marshal(gameMapResponse)
+	if err != nil {
+		return nil, err
+	}
+	messageResponse := &message.Message{
+		Type:    message.MessageType_MAP_RESPONSE,
+		Content: content,
+	}
+	return proto.Marshal(messageResponse)
+}
+
+func (fr *FrameRunner) BroadcastMap() {
+	log.Info("Start Broadcast Map")
+	data, err := fr.prepareMapResponse()
+	if err != nil {
+		panic(err)
+	}
 	for i := 0; i < len(fr.Players); i++ {
-		fr.Players[i].SendMap(fr.Map)
+		fr.Players[i].SendMessage(data)
+		log.Info(fmt.Sprintf("Broadcast map to %s", fr.Players[i]))
+	}
+}
+
+func (fr *FrameRunner) AddPlayer(player player.IPlayer) {
+	fr.Players = append(fr.Players, player)
+}
+
+func (fr *FrameRunner) Run() {
+	ticker := time.NewTicker(1 * time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			fr.BroadcastMap()
+		}
 	}
 }
--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
-	"github.com/aleksl0l/bomb-backend/message"
-	"github.com/golang/protobuf/proto"
+	"github.com/aleksl0l/bomb-backend/frame-runner"
 	"github.com/gorilla/websocket"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"net/http"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin:     func(r *http.Request) bool { return true },
+	}
+	frameRunner = frame_runner.NewFrameRunner()
 )
 
 func init() {
@@ -18,55 +26,11 @@ func init() {
 	}
 }
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
-}
-
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		return
-	}
-	for {
-		_, data, err := conn.ReadMessage()
-		if err != nil {
-			return
-		}
-		msg := &message.Message{}
-		err = proto.Unmarshal(data, msg)
-		if err != nil {
-			return
-		}
-		switch msg.Type {
-		case message.MessageType_JOIN_GAME:
-			handleJoinGame(msg)
-		case message.MessageType_LEAVE_GAME:
-			handlerLeaveGame(msg)
-		}
-		log.Info("read done")
-	}
-}
-
-func handleJoinGame(msg *message.Message) error {
-	joinGame := &message.JoinGame{}
-	err := proto.Unmarshal(msg.Content, joinGame)
-	if err != nil {
-		return err
-	}
-	log.Info(fmt.Sprintf("User %s join game", joinGame.Username))
-	return nil
-}
-
-func handlerLeaveGame(msg *message.Message) error {
-	return nil
-}
-
 func main() {
 	http.HandleFunc("/ws", serveWs)
 	addr := viper.GetString("serverAddress")
 	log.Info(fmt.Sprintf("Start serving on %s", addr))
+	go frameRunner.Run()
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
